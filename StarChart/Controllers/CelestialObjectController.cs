fa47a14a861a9e67c2376fc54314f2a5d8c9c114
mark.cabana <mark.cabana@first.com.au>
2021-05-03 14:44:56 +1000

    - step 4

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,5 +17,40 @@ namespace StarChart.Controllers
         {
             this._context = applicationDbContext;
         }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.FirstOrDefault(c => c.Id == id);
+            if (celestialObject == null) return NotFound();
+            celestialObject.Satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId.HasValue && c.OrbitedObjectId.Value == id)
+                .ToList();
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("{name}", Name = "GetById")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObjects = _context.CelestialObjects.Where(c => c.Name == name).ToList();
+            if(!(celestialObjects?.Any() ?? false))
+            {
+                return NotFound();
+            }
+            celestialObjects.ForEach(o => {
+                o.Satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId.HasValue && c.OrbitedObjectId.Value == o.Id).ToList();
+            });
+            return Ok(celestialObjects);
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = _context.CelestialObjects.ToList();
+            celestialObjects.ForEach(o => {
+                o.Satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId.HasValue && c.OrbitedObjectId.Value == o.Id).ToList();
+            });
+            return Ok(celestialObjects);
+        }
     }
 }
